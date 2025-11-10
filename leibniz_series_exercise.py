2b282bb8ef5508a78f6e81273238f0cf2df37649
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-11-10 15:01:18 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,7 +3,6 @@ def approximate_pi(n_terms):
     for i in range(n_terms):
         term = ((-1)**i) / (2 * i + 1)
         leibniz_series.append(term)
-    
     sum_leibniz_series = sum(leibniz_series)
     pi_approximation = 4 * sum_leibniz_series
     return pi_approximation
