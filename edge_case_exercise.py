1d1c6866a0f6f51868a9956b843b74678128967c
Tzvi Sender <gr58cw5c59@privaterelay.appleid.com>
2025-11-10 15:21:09 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,8 +1,4 @@
 def move(my_list, direction=None):
-    # Finds the index of the one in the list
-    index_of_one = my_list.index(1)
-
-    # Move the one to the left or to the right
     if direction == 'right':
       if index_of_one == -1:
         return my_list
